923a9fde996bd96599bac967d69675b6a3cc4419
YangZheYu1219 <ji3g420132@yahoo.com.tw>
2022-05-26 18:23:39 +0800

    commit2

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import AccountView from '../views/accountView.vue'
 import HomeView from '../views/HomeView.vue'
 import CartView from '../views/CartView.vue'
@@ -22,7 +22,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(),
   routes
 })
 
